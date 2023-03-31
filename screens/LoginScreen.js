6570b6483c12e6d1ae0f445794e730f11b6ea53f
danlaanh202 <mushroomlazy259@gmail.com>
2023-04-01 04:54:03 +0700

    async storage the same as localstorage

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,7 +1,32 @@
+import { useContext, useState } from "react";
 import AuthContent from "../components/Auth/AuthContent";
+import { login } from "../utils/auth";
+import { Alert } from "react-native";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { AuthContext } from "../store/auth-context";
 
 function LoginScreen() {
-  return <AuthContent isLogin />;
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+  const authCtx = useContext(AuthContext);
+
+  const loginHandler = async ({ email, password }) => {
+    setIsAuthenticating(true);
+    try {
+      const token = await login(email, password);
+      console.log(token);
+      authCtx.authenticate(token);
+    } catch (error) {
+      Alert.alert(
+        "Authentication failed!",
+        "Could not login. Please check again"
+      );
+    }
+    setIsAuthenticating(false);
+  };
+  if (isAuthenticating) {
+    return <LoadingOverlay message="Logging in.." />;
+  }
+  return <AuthContent isLogin onAuthenticate={loginHandler} />;
 }
 
 export default LoginScreen;
--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,24 @@
 import { StyleSheet, Text, View } from "react-native";
-
+import axios from "axios";
+import { useEffect, useState, useContext } from "react";
+import { AuthContext } from "../store/auth-context";
 function WelcomeScreen() {
+  const [fetchedMessage, setFetchedMessage] = useState("");
+  const authCtx = useContext(AuthContext);
+  const token = authCtx.token;
+  useEffect(() => {
+    axios
+      .get(
+        "https://react-native-3cd1d-default-rtdb.asia-southeast1.firebasedatabase.app/message.json?auth=" +
+          token
+      )
+      .then((response) => setFetchedMessage(response.data));
+  }, [token]);
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
     </View>
   );
 }
